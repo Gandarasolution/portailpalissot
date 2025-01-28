ff5990b8f54d1b55e1b762dea12d8759c4b684f2
bryan parmentelot <contact@bryan-is-here.com>
2025-01-28 16:39:21 +0100

    Bryan - Scroll top

---
+++ src/components/commun/ScrollToTopButton.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+
+function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 10) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button onClick={scrollToTop} className="scroll-to-top-button">
+        <FontAwesomeIcon icon={faArrowUp} size="lg" />
+      </button>
+      )}
+    </>
+  );
+}
+
+export default ScrollToTopButton;
